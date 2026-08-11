93c1eea6a559485b7825a11fb3e9a5136cb02918
Diego Gutierrez Martinez <diegogutierrezmartinez@MacBook-Pro-de-Diego.local>
2026-08-11 18:24:54 +0200

    Corrige cronologia y claridad de mapas de saga

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-const CACHE_VERSION = '20260811-saga-map6';
+const CACHE_VERSION = '20260811-saga-map7';
 const SHELL_CACHE = `neoanimez-shell-${CACHE_VERSION}`;
 const DATA_CACHE = `neoanimez-data-${CACHE_VERSION}`;
 
